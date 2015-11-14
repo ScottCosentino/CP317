74bf4adbb2e43a51e653d318b6c19fa240ce3d92
matt2405warner <mattwarner@rogers.com>
2015-11-14 13:54:46 -0500

    more css

--- php/portfolio.php
+++ php/portfolio.php
@@ -1,23 +1,20 @@
-<div>
-	<!-- Nav Tabs -->
-	<ul class="nav nav-tabs" role="tablist">
-		<li role="presentation" class="active">
-			<a href="#myStocks" aria-controls="myStocks" role="tab" data-toggle="tab">
-				My Stocks
-			</a>
-		</li>
-		<li role="presentation">
-			<a href="#myStats" aria-controls="myStats" role="tab" data-toggle="tab">
-				My Stats
-			</a>
-		</li>
-	</ul>
-	<!-- Tab Panes -->
-	<div class="tab-content">
-		<div role="tabpanel" class="tab-pane active" id="myStocks">
-			<div id="container" style="width:50%; height:500px;"></div>
-		</div> <!-- /.myStocks -->
-		<div role="tabpanel" class="tab-pane" id="myStats">
-		</div> <!-- /.myStats -->
+<div class="col-xs-12">
+	<div class="row portfolio-first-row">
+		<button type="button" class="btn btn-primary pull-right">
+			<span class="glyphicon glyphicon-wrench">
+				Settings
+			</span>
+		</button>
+		<button type="button" class="settings-btn btn btn-success pull-right">
+			<span class="glyphicon glyphicon-plus">
+				Add New Stock
+			</span>
+		</button>
+	</div>
+	<div class="row">
+		<input type="text" class="form-control stock-search-input" style="color:white;" placeholder="Search..." aria-desrcibedby="basic-addon1">
+	</div>
+	<div class="row">
+		<div id="container" style="width:30%; height:400px;"></div>
 	</div>
 </div>
